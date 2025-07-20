26c0b853cc778e89170782a1889def018b313a75
beenvyn <beenvyn5262@gmail.com>
2025-07-21 00:37:05 +0900

    Feat: 테스트 페이지 생성

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,6 +1,5 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
-  <main>hi</main>
+  <main>main</main>
 </template>
---
+++ src/pages/TestPage.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Head1 from '@/shared/components/atoms/typography/Head1.vue';
+import Head2 from '@/shared/components/atoms/typography/Head2.vue';
+import Caption1 from '@/shared/components/atoms/typography/Caption1.vue';
+</script>
+
+<template>
+  <Head1>Head1입니다</Head1>
+  <Head2>Head2입니다</Head2>
+  <Caption1>Caption1 입니다.</Caption1>
+</template>
