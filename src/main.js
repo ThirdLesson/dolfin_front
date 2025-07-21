c00be7280b060c19d518ac0be6b9e17832d84802
beenvyn <beenvyn5262@gmail.com>
2025-07-21 16:13:25 +0900

    Chore: Bootstrap-icons 설치

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { createPinia } from 'pinia';
 import App from './app/App.vue';
 import router from './app/router';
 import './shared/styles/index.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const app = createApp(App);
 
