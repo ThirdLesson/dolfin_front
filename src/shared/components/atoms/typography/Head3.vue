a57968132393c44e452a5bdf6fcc11bb28fbebba
Uyewon <dpdnns777@gmail.com>
2025-07-21 09:37:13 +0900

    Feat: 타이포그래피 컴포넌트 생성

---
+++ src/shared/components/atoms/typography/Head3.vue
@@ -0,0 +1,15 @@
+<script setup>
+import BaseTypography from './BaseTypography.vue';
+
+const props = defineProps({
+  className: {
+    type: String,
+  },
+});
+</script>
+
+<template>
+  <BaseTypography type="Head3" :class="className">
+    <slot />
+  </BaseTypography>
+</template>
---
+++ src/shared/components/atoms/typography/P2.vue
@@ -0,0 +1,15 @@
+<script setup>
+import BaseTypography from './BaseTypography.vue';
+
+const props = defineProps({
+  className: {
+    type: String,
+  },
+});
+</script>
+
+<template>
+  <BaseTypography type="P2" :class="className">
+    <slot />
+  </BaseTypography>
+</template>
