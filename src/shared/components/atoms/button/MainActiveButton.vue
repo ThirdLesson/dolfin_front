5393a4013b116f01713bbc9c3271b5d330e900ec
Uyewon <dpdnns777@gmail.com>
2025-07-21 11:07:24 +0900

    Feat: 버튼 공용 컴포넌트 생성

---
+++ src/shared/components/atoms/button/MainActiveButton.vue
@@ -0,0 +1,14 @@
+<script setup>
+import BaseButton from './BaseButton.vue';
+import Subtitle1 from '../typography/Subtitle1.vue';
+
+const emit = defineEmits(['click']);
+</script>
+
+<template>
+  <BaseButton size="large" variant="primary" @click="emit('click', $event)">
+    <Subtitle1>
+      <slot />
+    </Subtitle1>
+  </BaseButton>
+</template>
