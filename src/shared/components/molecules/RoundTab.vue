e6b05918fb20ccb318373c84d1c5553c441a6f5a
beenvyn <beenvyn5262@gmail.com>
2025-07-22 16:46:50 +0900

    Feat: 공용 Tab 컴포넌트 생성

---
+++ src/shared/components/molecules/RoundTab.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { computed } from 'vue';
+import P1 from '../atoms/typography/P1.vue';
+
+const model = defineModel();
+const props = defineProps({
+  opt1: {
+    type: String,
+    required: true,
+  },
+  opt2: {
+    type: String,
+    required: true,
+  },
+});
+
+const select = (option) => {
+  model.value = option;
+};
+
+const selectedOption = computed(() => {
+  return model.value;
+});
+</script>
+
+<template>
+  <div class="h-[35px] w-fit rounded-md bg-dol-sub flex p-[5px] select-none">
+    <div
+      class="flex-1 flex px-4 items-center justify-center rounded-md cursor-pointer transition-all"
+      :class="selectedOption === props.opt1 ? 'bg-white' : 'text-dol-dark-gray'"
+      @click="select(props.opt1)"
+    >
+      <P1 class="whitespace-nowrap">{{ props.opt1 }}</P1>
+    </div>
+
+    <div
+      class="flex-1 flex px-4 items-center justify-center rounded-md cursor-pointer transition-all"
+      :class="selectedOption === props.opt2 ? 'bg-white' : 'text-dol-dark-gray'"
+      @click="select(props.opt2)"
+    >
+      <P1 class="whitespace-nowrap">{{ props.opt2 }}</P1>
+    </div>
+  </div>
+</template>
---
+++ src/shared/components/molecules/SquareTab.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { computed } from 'vue';
+import P1 from '../atoms/typography/P1.vue';
+
+const model = defineModel();
+const props = defineProps({
+  options: {
+    type: Array,
+    default: () => [],
+  },
+});
+
+const select = (option) => {
+  model.value = option;
+};
+
+const selectedOption = computed(() => {
+  return model.value;
+});
+</script>
+<template>
+  <div class="w-full flex flex-wrap gap-2">
+    <button
+      v-for="option in props.options"
+      :key="option.value"
+      @click="select(option.value)"
+      class="h-[35px] px-9 flex justify-center items-center border-2 rounded-sm"
+      :class="
+        selectedOption === option.value
+          ? 'bg-dol-sub border-dol-main'
+          : 'border-dol-sub '
+      "
+    >
+      <P1 class="text-dol-dark whitespace-nowrap">{{ option.label }}</P1>
+    </button>
+  </div>
+</template>
