6576501805a98b5affabb9c7116efd423181b735
beenvyn <beenvyn5262@gmail.com>
2025-07-22 23:56:28 +0900

    Feat: Header 공용 컴포넌트 생성

---
+++ src/shared/components/molecules/Header.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { useRouter } from 'vue-router';
+
+import Head2 from '../atoms/typography/Head2.vue';
+import chevron from '@/asset/chevron-left.png';
+
+const router = useRouter();
+const goBack = () => router.back();
+</script>
+
+<template>
+  <div class="w-full flex items-center gap-5">
+    <div class="cursor-pointer flex justify-center items-center">
+      <img :src="chevron" @click="goBack" />
+    </div>
+    <Head2>
+      <slot />
+    </Head2>
+  </div>
+</template>
