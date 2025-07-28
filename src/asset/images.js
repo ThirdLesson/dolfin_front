b9e0cb2c3cde0ed3354a2dacb54ea2aece6e7372
beenvyn <beenvyn5262@gmail.com>
2025-07-28 17:26:11 +0900

    Chore: 이미지 import 파일 추가

---
+++ src/asset/images.js
@@ -0,0 +1,113 @@
+import busan from './bank/busan.png';
+import city from './bank/city.png';
+import cu from './bank/cu.png';
+import dgb from './bank/dgb.png';
+import gwangju from './bank/gwangju.png';
+import hana from './bank/hana.png';
+import ibk from './bank/ibk.png';
+import im from './bank/im.png';
+import k from './bank/k.png';
+import kakao from './bank/kakao.png';
+import kb from './bank/kb.png';
+import kdb from './bank/kdb.png';
+import mg from './bank/mg.png';
+import nh from './bank/nh.png';
+import post from './bank/post.png';
+import sc from './bank/SC.png';
+import sh from './bank/sh.png';
+import shinhan from './bank/shinhan.png';
+import toss from './bank/toss.png';
+import woori from './bank/woori.png';
+
+import canada from './flag/canada.png';
+import china from './flag/china.png';
+import eu from './flag/eu.png';
+import hongkong from './flag/hongkong.png';
+import indonesia from './flag/indonesia.png';
+import japan from './flag/japan.png';
+import korea from './flag/korea.png';
+import malaysia from './flag/malaysia.png';
+import russia from './flag/russia.png';
+import thailand from './flag/thailand.png';
+import uk from './flag/uk.png';
+import usa from './flag/usa.png';
+import vietnam from './flag/vietnam.png';
+
+import calculator from './icon/calculator.png';
+import check from './icon/check.png';
+import dutchpay from './icon/dutchpay.png';
+import exchange from './icon/exchange.png';
+import exchangeActive from './icon/exchangeActive.png';
+import main from './icon/main.png';
+import mainActive from './icon/mainActive.png';
+import map from './icon/map.png';
+import mapActive from './icon/mapActive.png';
+import recommend from './icon/recommend.png';
+import remit from './icon/remit.png';
+import remitActive from './icon/remitActive.png';
+
+import circleLogo from './logo/circleLogo.png';
+import comingLogo from './logo/comingLogo.png';
+import loading from './logo/loading.gif';
+import logo from './logo/logo.png';
+
+export const Logos = {
+  circleLogo,
+  comingLogo,
+  loading,
+  logo,
+};
+
+export const Icons = {
+  calculator,
+  check,
+  dutchpay,
+  exchange,
+  exchangeActive,
+  main,
+  mainActive,
+  map,
+  mapActive,
+  recommend,
+  remit,
+  remitActive,
+};
+
+export const Flags = {
+  canada,
+  china,
+  eu,
+  hongkong,
+  indonesia,
+  japan,
+  korea,
+  malaysia,
+  russia,
+  thailand,
+  uk,
+  usa,
+  vietnam,
+};
+
+export const Banks = {
+  busan,
+  city,
+  cu,
+  dgb,
+  gwangju,
+  hana,
+  ibk,
+  im,
+  k,
+  kakao,
+  kb,
+  kdb,
+  mg,
+  nh,
+  post,
+  sc,
+  sh,
+  shinhan,
+  toss,
+  woori,
+};
