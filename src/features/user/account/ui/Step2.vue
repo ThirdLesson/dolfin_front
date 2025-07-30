dafcf9952b25bfe3cae571b623b094deec83ccee
beenvyn <beenvyn5262@gmail.com>
2025-07-30 22:53:32 +0900

    feat: 계좌 등록 api 연동

--- src/features/user/account/ui/Step2.vue
+++ src/features/user/account/ui/Step2.vue
@@ -1,22 +1,29 @@
 <script setup>
-import { ref, onMounted, onUnmounted, computed, watch } from 'vue';
+import { ref, onMounted, onUnmounted, computed } from 'vue';
 import Subtitle1 from '@/shared/components/atoms/typography/Subtitle1.vue';
 import Subtitle2 from '@/shared/components/atoms/typography/Subtitle2.vue';
 import Caption1 from '@/shared/components/atoms/typography/Caption1.vue';
 import BoxInput from '@/shared/components/atoms/input/BoxInput.vue';
 import LgMainButton from '@/shared/components/atoms/button/LgMainButton.vue';
 import PwModal from '@/shared/components/organisms/PwModal.vue';
+import { verifyCode } from '../services/account.service';
 
 const emit = defineEmits(['complete']);
+const props = defineProps({
+  authCode: {
+    type: String,
+    default: '',
+  },
+});
 
-const authCode = ref('');
 const password = ref('');
 const passwordCheck = ref('');
 const isModalOpen = ref(false);
 const isCheckModalOpen = ref(false);
 const showError = ref(false);
+const inputAuthCode = ref(props.authCode);
 
-const time = ref(180); // 3분 = 180초
+const time = ref(300);
 let timer;
 
 const formattedTime = computed(() => {
@@ -25,24 +32,36 @@ const formattedTime = computed(() => {
   return `${minutes}:${seconds}`;
 });
 
+const handleComplete = async () => {
+  const result = await verifyCode({
+    authCode: Number(inputAuthCode.value),
+  });
+
+  if (result.status === 204) {
+    isModalOpen.value = true;
+  }
+};
+
 const handlePwComplete = (pw) => {
   password.value = pw;
   isModalOpen.value = false;
   isCheckModalOpen.value = true;
 };
 
-const handleComplete = (pw) => {
+const handlePwCheckComplete = (pw) => {
   passwordCheck.value = pw;
 
   if (password.value === passwordCheck.value) {
     isCheckModalOpen.value = false;
-    emit('complete');
+    emit('complete', password.value);
   } else {
     showError.value = true;
   }
 };
 
-const isValid = computed(() => time.value !== 0 && authCode.value.length === 4);
+const isValid = computed(
+  () => time.value !== 0 && inputAuthCode.value.length === 4,
+);
 
 onMounted(() => {
   timer = setInterval(() => {
@@ -71,7 +90,7 @@ onUnmounted(() => {
       <div class="w-full flex flex-col items-end gap-1">
         <div class="w-full flex flex-col gap-[10px]">
           <Subtitle2>입금자명</Subtitle2>
-          <BoxInput v-model="authCode" />
+          <BoxInput v-model="inputAuthCode" />
         </div>
         <Caption1 class="text-dol-error"
           >인증시간 {{ formattedTime }} 남음</Caption1
@@ -81,7 +100,7 @@ onUnmounted(() => {
         <Caption1 class="underline text-dol-dark-gray"
           >데모버전에서는 1원 인증 기능을 제공하지 않습니다.</Caption1
         >
-        <LgMainButton :disabled="!isValid" @click="() => (isModalOpen = true)"
+        <LgMainButton :disabled="!isValid" @click="handleComplete"
           >완료</LgMainButton
         >
       </div>
@@ -99,7 +118,7 @@ onUnmounted(() => {
     errorMessage="비밀번호가 일치하지 않습니다."
     :showError="showError"
     @input="showError = false"
-    @complete="handleComplete"
+    @complete="handlePwCheckComplete"
     @close="
       () => {
         isCheckModalOpen = false;
