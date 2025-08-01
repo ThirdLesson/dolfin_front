421a408cfbbd9af512d5e5860500a55462ff8d3b
beenvyn <beenvyn5262@gmail.com>
2025-08-01 16:06:20 +0900

    Chore: pages 폴더 구조 변경

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.fvpg82c1egg"
+    "revision": "0.9279bbr9vlg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
