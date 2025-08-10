ab1c9fb2ac2ae39086b53472acdcd540cc0fdc98
beenvyn <beenvyn5262@gmail.com>
2025-08-10 20:19:01 +0900

    Feat: 적금 api 연동

--- src/pages/recommendation/RecommendationPage.vue
+++ src/pages/recommendation/RecommendationPage.vue
@@ -1,12 +1,13 @@
 <script setup>
 import { ref } from 'vue';
 import RecommendationHeader from '@/features/recommendation/ui/RecommendationHeader.vue';
-import ProductList from '@/features/recommendation/ui/ProductList.vue';
 import ProductInfoModal from '@/features/recommendation/ui/ProductInfoModal.vue';
+import DepositList from '@/features/recommendation/ui/DepositList.vue';
+import SavingList from '@/features/recommendation/ui/SavingList.vue';
+import PersonalLoanList from '@/features/recommendation/ui/PersonalLoanList.vue';
+import JeonseLoanList from '@/features/recommendation/ui/JeonseLoanList.vue';
 
-const productOption = ref('예금');
-const periodOption = ref('STAY_EXPIRATION');
-const conditionOption = ref([]);
+const productOption = ref('DEPOSIT');
 const showFilterModal = ref(false);
 const showInfoModal = ref(false);
 const selectedProduct = ref(null);
@@ -21,20 +22,37 @@ const openInfoModal = (product) => {
   <div class="flex flex-col min-h-screen gap-[15px]">
     <RecommendationHeader
       v-model:productOption="productOption"
-      v-model:periodOption="periodOption"
-      v-model:conditionOption="conditionOption"
-      v-model:showFilterModal="showFilterModal"
+      @click="showFilterModal = true"
     />
 
-    <ProductList
-      :productOption="productOption"
-      :periodOption="periodOption"
-      :conditionOption="conditionOption"
+    <DepositList
+      v-if="productOption === 'DEPOSIT'"
+      :showModal="showFilterModal"
       @select="openInfoModal"
+      @close="showFilterModal = false"
+    />
+    <SavingList
+      v-else-if="productOption === 'SAVING'"
+      :showModal="showFilterModal"
+      @select="openInfoModal"
+      @close="showFilterModal = false"
+    />
+    <PersonalLoanList
+      v-else-if="productOption === 'PERSONAL_LOAN'"
+      :showModal="showFilterModal"
+      @select="openInfoModal"
+      @close="showFilterModal = false"
+    />
+    <JeonseLoanList
+      v-else-if="productOption === 'JEONSE_LOAN'"
+      :showModal="showFilterModal"
+      @select="openInfoModal"
+      @close="showFilterModal = false"
     />
 
     <ProductInfoModal
       :show="showInfoModal"
+      :productOption="productOption"
       :product="selectedProduct"
       @close="showInfoModal = false"
     />
