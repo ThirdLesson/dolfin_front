dc5fb37f0de18bb66cf448d3830257433432f581
beenvyn <beenvyn5262@gmail.com>
2025-08-10 20:17:04 +0900

    refactor: 예금 추천 컴포넌트 분리

--- src/features/recommendation/services/recommendation.service.js
+++ src/features/recommendation/services/recommendation.service.js
@@ -33,3 +33,88 @@ export async function getDepositInfo(depositId) {
   const res = await response.json();
   return res;
 }
+
+export async function getSavingsFilter({
+  productPeriod,
+  page,
+  pageSize = 20,
+  spclConditions,
+}) {
+  const { url, method } = recommendation.savingsFilter();
+
+  const params = new URLSearchParams({
+    productPeriod,
+    page,
+    pageSize,
+  });
+
+  spclConditions?.forEach((cond) => params.append('spclConditions', cond));
+
+  const queryUrl = `${url}?${params.toString()}`;
+  const response = await apiFetch(queryUrl, { method });
+
+  const res = await response.json();
+  return res;
+}
+
+export async function getSavingInfo(savingId) {
+  const { url, method } = recommendation.savingsInfo();
+  const queryUrl = `${url}/${savingId}`;
+
+  const response = await apiFetch(queryUrl, { method });
+
+  const res = await response.json();
+  return res;
+}
+
+export async function getPersonalLoanFilter({
+  page,
+  pageSize = 20,
+  filterType,
+  minAmount,
+  maxAmount,
+}) {
+  const { url, method } = recommendation.personalLoanFilter();
+
+  const params = new URLSearchParams({
+    page,
+    pageSize,
+    filterType,
+    minAmount,
+    maxAmount,
+  });
+
+  const queryUrl = `${url}?${params.toString()}`;
+  const response = await apiFetch(queryUrl, { method });
+
+  const res = await response.json();
+  return res;
+}
+
+export async function getPersonalLoanInfo(personalLoanId) {
+  const { url, method } = recommendation.personalLoanInfo();
+  const queryUrl = `${url}/${personalLoanId}`;
+
+  const response = await apiFetch(queryUrl, { method });
+
+  const res = await response.json();
+  return res;
+}
+
+export async function getJeonseLoanFilter({ page, pageSize = 20, filterType }) {
+  const { url, method } = recommendation.jeonseLoanFilter();
+
+  const params = new URLSearchParams({
+    page,
+    pageSize,
+    filterType,
+    minAmount,
+    maxAmount,
+  });
+
+  const queryUrl = `${url}?${params.toString()}`;
+  const response = await apiFetch(queryUrl, { method });
+
+  const res = await response.json();
+  return res;
+}
