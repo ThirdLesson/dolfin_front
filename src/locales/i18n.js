3e342b6f94fabab1ebe7f65c4a91de338f88e302
Uyewon <dpdnns777@gmail.com>
2025-08-12 16:48:08 +0900

    Feat: 중국어 UI 적용

--- src/locales/i18n.js
+++ src/locales/i18n.js
@@ -1,10 +1,18 @@
 import { createI18n } from 'vue-i18n';
 import messages from '@/locales';
 
+const supportedLocales = ['en', 'ko', 'jp', 'zh'];
+
+function getBrowserLocale() {
+  const lang = navigator.language || navigator.userLanguage;
+  const shortLang = lang.split('-')[0];
+  return supportedLocales.includes(shortLang) ? shortLang : 'ko';
+}
+
 export default createI18n({
   legacy: false,
   globalInjection: true,
-  locale: 'ko',
-  fallbackLocale: 'en',
+  locale: getBrowserLocale(),
+  fallbackLocale: 'ko',
   messages,
 });
